2e47a4596b4e9b130247ef20b0a3a4ad69dd6baf
Neylanio Soares <neylaniogomes@gmail.com>
2020-06-09 18:48:37 -0300

    To reverse update folders about deploying on heroku

--- src/controllers/PointsController.ts
+++ src/controllers/PointsController.ts
@@ -20,8 +20,7 @@ class PointsController {
         const serializedPoints = points.map(point => { 
             return{
                 ...point,
-                // image_url: `http://192.168.100.8:3333/uploads/${point.image}`
-                image_url: `https://e-coleta-backend.herokuapp.com/uploads/${point.image}`
+                image_url: `http://192.168.100.8:3333/uploads/${point.image}`
             }
         });
 
@@ -35,8 +34,7 @@ class PointsController {
 
         const serializedPoint = { 
             ...point,
-            // image_url: `http://192.168.100.8:3333/uploads/${point.image}`
-            image_url: `https://e-coleta-backend.herokuapp.com/uploads/${point.image}`
+            image_url: `http://192.168.100.8:3333/uploads/${point.image}`
         };
 
         const items = await knex('items')
