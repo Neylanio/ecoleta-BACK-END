88aae2c0e3122c4a3ced3e605a199b3db3d5cef3
Neylanio Soares <neylaniogomes@gmail.com>
2020-06-09 18:14:23 -0300

    To deploy at heroku

--- src/controllers/PointsController.ts
+++ src/controllers/PointsController.ts
@@ -20,7 +20,8 @@ class PointsController {
         const serializedPoints = points.map(point => { 
             return{
                 ...point,
-                image_url: `http://192.168.100.8:3333/uploads/${point.image}`
+                // image_url: `http://192.168.100.8:3333/uploads/${point.image}`
+                image_url: `https://e-coleta-backend.herokuapp.com/uploads/${point.image}`
             }
         });
 
@@ -34,7 +35,8 @@ class PointsController {
 
         const serializedPoint = { 
             ...point,
-            image_url: `http://192.168.100.8:3333/uploads/${point.image}`
+            // image_url: `http://192.168.100.8:3333/uploads/${point.image}`
+            image_url: `https://e-coleta-backend.herokuapp.com/uploads/${point.image}`
         };
 
         const items = await knex('items')
